a7ca3cb815b4e6422182deec5f6f2f44b3827e7c
Luifer1994 <almendralesluifer@gmail.com>
2021-08-10 22:56:32 -0500

    create controller and migrate tipos personas

--- database/migrations/2021_08_11_023737_clientes.php
+++ database/migrations/2021_08_11_023737_clientes.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Clients extends Migration
+class Clientes extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class Clients extends Migration
      */
     public function up()
     {
-        Schema::create('clients', function (Blueprint $table) {
+        Schema::create('clientes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -33,6 +33,6 @@ class Clients extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('clients');
+        Schema::dropIfExists('clientes');
     }
 }
